2a620b13891f033c1dc725f02a858bb3ba7480e2
Johannes <johtso@gmail.com>
2021-06-30 22:35:07 +0100

    No webvitals

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import { debugContextDevtool } from 'react-context-devtool';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root');
 
@@ -20,4 +19,4 @@ debugContextDevtool(container);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+// reportWebVitals(console.log);
